ab42592edcb69be62827d02fe8a4969288e93ade
Vortex856 <omparam03@gmail.com>
2025-10-19 22:03:12 +0530

    aaa

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const projectsToggle = document.getElementById('projectsToggle');
+  const profileMain = document.getElementById('profileMain');
+  const projectsContainer = document.querySelector('.projectsContainer');
+  const backBtn = document.querySelector('.backBtn');
+  const projectsList = document.querySelector('.projectsList');
+
+  const projects = [
+    {
+      title: 'Nothing here yet.',
+      description: 'Maybe one day i will get my lazy self to code something.',
+      link: '/'
+    }
+  ];
+
+  function createProjectsList() {
+    projectsList.innerHTML = '';
+
+    projects.forEach(project => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <h3 class="projectTitle">${project.title}</h3>
+        <p class="projectDescription">${project.description}</p>
+      `;
+      li.addEventListener('click', () => {
+        window.location.href = project.link;
+      });
+      projectsList.appendChild(li);
+    });
+
+    profileMain.classList.add('hidden');
+    projectsContainer.style.display = 'block';
+  }
+
+  function resetView() {
+    profileMain.classList.remove('hidden');
+    projectsContainer.style.display = 'none';
+  }
+
+  projectsToggle.addEventListener('click', createProjectsList);
+  backBtn.addEventListener('click', resetView);
+});
